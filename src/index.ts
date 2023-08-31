f9da21701e4607fcf61f8bbef99fb7cb96a320f7
Jan Kaul <jankaul@mailbox.org>
2023-08-31 16:57:45 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { TableMetadata } from "./TableMetadata"
+export { Schema } from "./Schema"
+export { PartitionSpec } from "./PartitionSpec"
+export { PartitionField } from "./PartitionField"
+export { StructType } from "./StructType"
+export { StructField } from "./StructField"
